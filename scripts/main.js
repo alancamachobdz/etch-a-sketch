24c2df9da8bbc4b5dba0c99c00cd62622d9eb5c5
Alan Camacho <alancamachobdz@gmail.com>
2020-05-06 14:52:54 -0700

    Reworked everything into functions, the Etch-a-Sketch is complete.

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,38 @@
 const container = document.querySelector('#container');
 
-for(i = 0; i < 256; i++) {
-    let grid = document.createElement('div');
-    grid.className = 'gridElements';
-    grid.setAttribute('id', 'gridNumber' + [i]);
+function createGrid(numberPerSide) {
+    for(i = 0; i < numberPerSide; i++) {
+        let grid = document.createElement('div');
+        grid.className = 'gridElements';
+        grid.setAttribute('id', 'gridNumber' + [i]);
 
-    container.appendChild(grid);
+        container.appendChild(grid);
+    }
 }
 
-const gridElementList = document.querySelectorAll('.gridElements');
 
-for(let j = 0; j < gridElementList.length; j++) {
-    gridElementList[j].addEventListener('mouseover', function() {
-        gridElementList[j].style.backgroundColor = 'black';
-    })
+function sketch() {
+    const gridElementList = document.querySelectorAll('.gridElements');
+    for(let j = 0; j < gridElementList.length; j++) {
+        gridElementList[j].addEventListener('mouseover', function() {
+            gridElementList[j].style.backgroundColor = 'black';
+        })
+    }
 }
 
 const button = document.querySelector('#btn');
 button.addEventListener('click', etchASketch)
 
 function etchASketch() {
-    let numberPerSide = Number(prompt('How many squares per side?'));
-    let numberForGrid = numberPerSide * numberPerSide
+    clearContent();
+    let numberForSide = Number(prompt('How many squares per side?'));
+    let numberPerSide = numberForSide * numberForSide;
+    createGrid(numberPerSide);
+    sketch();
+}
+
+function clearContent() {
+    let content = document.getElementById('container');
+    content.innerHTML = '';
 }
 
