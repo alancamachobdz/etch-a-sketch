1c59b4cb1f0c953da2878cfbbac2ebe1c1d2d2a3
Alan Camacho <alancamachobdz@gmail.com>
2020-05-06 13:57:21 -0700

    Created loop for each grid element, when hovered over each grid element the background color of the element will change to black.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,16 @@ const container = document.querySelector('#container');
 for(i = 0; i < 256; i++) {
     let grid = document.createElement('div');
     grid.className = 'gridElements';
+    grid.setAttribute('id', 'gridNumber' + [i]);
 
     container.appendChild(grid);
 }
+
+const gridElementList = document.querySelectorAll('.gridElements');
+console.log(gridElementList);
+for(let j = 0; j < gridElementList.length; j++) {
+    gridElementList[j].addEventListener('mouseover', function() {
+        gridElementList[j].style.backgroundColor = 'black';
+    })
+}
+
